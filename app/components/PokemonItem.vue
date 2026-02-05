047d3e2a2f6d9e01a55e1a34a55d9ee07e6c16f3
Guillaume Chau <guillaume.b.chau@gmail.com>
2026-02-05 12:54:48 +0100

    feat: nicer pokemon image loading UX

--- app/components/PokemonItem.vue
+++ app/components/PokemonItem.vue
@@ -5,6 +5,27 @@ const { pokemon } = defineProps<{
 
 const editOpen = ref(false)
 
+const imageLoaded = ref(false)
+const imageError = ref(false)
+
+const showFallback = computed(() => !pokemon.sprite || imageError.value)
+
+watch(
+  () => pokemon.sprite,
+  () => {
+    imageLoaded.value = false
+    imageError.value = false
+  },
+)
+
+function onImageLoad() {
+  imageLoaded.value = true
+}
+
+function onImageError() {
+  imageError.value = true
+}
+
 const ago = useTimeAgo(() => pokemon.updatedAt)
 </script>
 
@@ -12,7 +33,25 @@ const ago = useTimeAgo(() => pokemon.updatedAt)
   <div
     class="p-2 sm:p-4 flex items-center gap-2 sm:gap-4 hover:bg-elevated/50 sm:flex-col sm:rounded-md"
   >
-    <img :key="pokemon.sprite" :src="pokemon.sprite" :alt="pokemon.name" class="size-20 sm:size-42">
+    <div class="relative size-20 sm:size-42 shrink-0 rounded-md overflow-hidden bg-neutral-900/40">
+      <img
+        v-if="!showFallback"
+        :key="pokemon.sprite"
+        :src="pokemon.sprite"
+        :alt="pokemon.name"
+        class="size-full object-contain transition-opacity duration-500 ease-out"
+        :class="imageLoaded ? 'opacity-100' : 'opacity-0'"
+        @load="onImageLoad"
+        @error="onImageError"
+      >
+
+      <div
+        v-else
+        class="absolute inset-0 flex items-center justify-center rounded-md bg-neutral-800/80 text-neutral-300 font-semibold text-xl"
+      >
+        ?
+      </div>
+    </div>
 
     <div class="flex-1 min-w-0 flex flex-col items-stretch sm:text-center">
       <div class="truncate text-lg sm:text-2xl">
